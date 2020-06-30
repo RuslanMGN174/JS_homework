26510673fc77a648a04f0af4f609367cba66e5e8
Vegas <rusland174@gmail.com>
2020-06-30 12:18:28 +0500

    add regex comment-text clearing

--- chapter15/src/Comments/NewComment.js
+++ chapter15/src/Comments/NewComment.js
@@ -1,27 +1,32 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./Style.css";
 import Context from "../Context";
 
 const NewComment = ({ comment }) => {
 
-  const {removeComment} = useContext(Context);
+  const { removeComment } = useContext(Context);
+  const clearText = text => {
+    return (
+      text.replace(/<!--[\s\S]*?--!?>/g, "").replace(/<\/?[a-z][^>]*(>|$)/gi, "")
+    )
+  }
 
-  return (
-    <li className="media-left">
-      <div className="media-body">
-        <div className="media-heading">
-          <div className="author">{comment.author}</div>
-          <div className="metadata">
-            <span className="date">{comment.date}</span>
-            <span className="devide"> | </span>
-            <span className="delete" onClick={removeComment.bind(null, comment.id)}> Удалить </span>
-          </div>
-        </div>
-        <div className="media-text text-justify">{comment.text}
+return (
+  <li className="media-left">
+    <div className="media-body">
+      <div className="media-heading">
+        <div className="author">{comment.author}</div>
+        <div className="metadata">
+          <span className="date">{comment.date}</span>
+          <span className="devide"> | </span>
+          <span className="delete" onClick={removeComment.bind(null, comment.id)}> Удалить </span>
         </div>
       </div>
-    </li>
-  )
+      <div className="media-text text-justify">{clearText(comment.text)}
+      </div>
+    </div>
+  </li>
+)
 };
 
 export default NewComment;
